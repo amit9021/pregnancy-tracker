963232706bf6df971722ed169bf0e8473d3f6e45
amit.senouf <amit9021@gmail.com>
2024-11-28 16:55:01 +0200

    styling

--- src/componnets/Footer.jsx
+++ src/componnets/Footer.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import styles from "../assets/styles/styles.module.css";
+import { FaFacebook, FaWhatsapp, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer>
-      <div>
-        <p>צרו קשר: 052-1234567</p>
-        <p>אימייל:</p>
-        <p>עוד דרכים ליצירת קשר </p>
-      </div>
+      <section className={styles.contact}>
+        <div>
+          <p>צרו קשר: 052-1234567</p>
+          <p>אימייל:</p>
+          <p>עוד דרכים ליצירת קשר </p>
+        </div>
+        <div className={styles.social}>
+          <p>עקבו אחרינו:</p>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://Whatsapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaWhatsapp />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+        </div>
+      </section>
       <div className={styles.footer}>
         <p>&copy; 2024 אילונה חלמר. כל הזכויות שמורות.</p>
       </div>
